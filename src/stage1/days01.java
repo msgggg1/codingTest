f0b97a55f185f1d16246d337d48295fcea17aa39
Minsun Kim <msggms1@gmail.com>
2025-02-27 22:00:47 +0900

    Intial commit

---
+++ src/stage1/days01.java
@@ -0,0 +1,34 @@
+package stage1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		double a;
+		double b;
+		
+		a = scanner.nextDouble();
+		b = scanner.nextDouble();
+		
+		System.out.println(a/b);
+	}
+
+}
+
+//[2] a,b 입력받아서 a+b 출력
+//Scanner scanner = new Scanner(System.in);
+//
+//int a;
+//int b;
+//
+//a = scanner.nextInt();
+//b = scanner.nextInt();
+//
+//System.out.println(a+b);
+
+
+//[1] Hello world 출력 
+//System.out.println("Hello World!");
