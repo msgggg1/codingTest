c7d12342ee96652cef036e146270b3efe335d04d
msgggg1 <msggms1@gmail.com>
2025-03-02 00:08:22 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		String Id = scanner.nextLine();
+		
+		System.out.println(Id + "??!");
+
+	} // main
+
+} // class
