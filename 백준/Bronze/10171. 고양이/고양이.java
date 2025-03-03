ce32733afd9e88b4549b0f40a69bd3b20027e5ac
msgggg1 <msggms1@gmail.com>
2025-03-03 22:32:54 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		//	\    /\ 
+		//	 )  ( ')
+		//	(  /  ) 
+		//	 \(__)| 
+		System.out.println("\\    /\\ ");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  ) ");
+		System.out.println(" \\(__)| ");
+
+	} // main
+
+} // class
