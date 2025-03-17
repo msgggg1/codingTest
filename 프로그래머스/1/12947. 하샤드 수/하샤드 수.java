eeff56da6da3ed1a2badab2147628c523fc05f9f
msgggg1 <msggms1@gmail.com>
2025-03-17 23:10:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -12,3 +12,27 @@ class Solution {
         		else return false;
     }
 }
+/*
+class Solution {
+	public boolean solution(int x) {
+		boolean answer = true;
+		int sum = String.valueOf(x).chars().map(ch->ch-'0').sum();
+		return x % sum ==0 ;
+	}
+}
+*/
+/*
+class Solution {
+	public boolean solution(int x) {
+		boolean answer = true;
+		String [] tmp = String.valueOf(x).split("");
+		int sum = 0;
+		for (String s : tmp) {
+			sum+=Integer.parseInt(s);
+			
+			if (x%sum==0) return true; 
+    		else return false;
+		}
+	}
+}
+*/
