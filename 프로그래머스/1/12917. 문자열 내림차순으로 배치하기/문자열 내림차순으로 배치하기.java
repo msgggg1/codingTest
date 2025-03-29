8c26de6587ac944057ed0d708ac2a78106c0a52d
msgggg1 <msggms1@gmail.com>
2025-03-29 17:59:14 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 5.52 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -8,3 +8,17 @@ class Solution {
         return Arrays.stream(s.split("")).sorted(Collections.reverseOrder()).collect(Collectors.joining());
     }
 }
+
+
+/*
+class Solution {
+    public String solution(String s) {
+        
+    	char [] str = s.toCharArray();
+    	Arrays.sort(str);
+    	
+    	return new StringBuilder(new String(str)).reverse().toString();
+        
+    }
+}
+*/
