c6a43065a0bbf2267b3a650509b25b272bb1f544
msgggg1 <msggms1@gmail.com>
2025-04-03 23:24:05 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.40 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Stack;
 
 // 내 풀이
 class Solution {
@@ -21,3 +23,38 @@ class Solution {
         return answer;
 	}
 }
+
+/* ArrayList 풀이
+class Solution {
+	public int[] solution(int []arr) {
+
+		ArrayList<Integer> al = new ArrayList<>();
+		int preNum = 10 ;
+		for (int num : arr) {
+			if (preNum != num) {
+				al.add(num);
+				preNum = num;
+			}
+		}
+		
+		int [] answer = new int [al.size()];
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = al.get(i).intValue();
+		}
+		return answer;
+	}
+}
+*/
+/* Stack 풀이
+class Solution {
+	public Stack solution(int []arr) {
+
+		Stack<Integer> s = new Stack<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (s.size()==0 || s.peek() != arr[i]) 
+				s.push(arr[i]);
+		}
+		return s;
+	}
+}
+*/
