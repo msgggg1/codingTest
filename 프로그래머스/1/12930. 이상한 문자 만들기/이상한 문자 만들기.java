77a1c694184c5f3477149a5d433d123ca3cd0bb9
msgggg1 <msggms1@gmail.com>
2025-04-27 11:07:21 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.11 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -7,9 +7,12 @@ class Solution {
         
         for (int i = 0; i < s.length(); i++) {
 			c = s.charAt(i);
-        	if (c == ' ') { sb.append(' '); index = 0 ;}
-        	else if (index %2 == 0) { sb.append(Character.toUpperCase(c)); index++;}
-        	else { sb.append(Character.toLowerCase(c)); index++;}
+//        	if (c == ' ') { sb.append(' '); index = 0 ;}
+//        	else if (index %2 == 0) { sb.append(Character.toUpperCase(c)); index++;}
+//        	else { sb.append(Character.toLowerCase(c)); index++;}
+			c = index%2==0 ? Character.toUpperCase(c) : Character.toLowerCase(c); 
+			sb.append(c);
+			index = c == ' ' ? 0 : ++ index;
         }
       
         return sb.toString();
