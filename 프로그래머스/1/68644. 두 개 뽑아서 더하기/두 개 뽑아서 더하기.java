6c489e36b83a06b29c28d04df4572b88aff517c0
msgggg1 <msggms1@gmail.com>
2025-05-06 15:38:05 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 8.43 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,22 +1,60 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
+// 내 풀이
 class Solution {
-    public int[] solution(int[] numbers) {
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        for (int i = 0; i < numbers.length; i++) {
+   public int[] solution(int[] numbers) {
+       ArrayList<Integer> al = new ArrayList<Integer>();
+       for (int i = 0; i < numbers.length; i++) {
 			for (int j = i+1; j < numbers.length; j++) {
 				if(!al.contains(numbers[i]+numbers[j])) al.add(numbers[i]+numbers[j]);
 			}
 		}
-        al.sort(null);
-        int [] answer = new int [al.size()];
-        Iterator<Integer> ir = al.iterator();
-        int i = 0;
-        while (ir.hasNext()) {
+       al.sort(null);
+       int [] answer = new int [al.size()];
+       Iterator<Integer> ir = al.iterator();
+       int i = 0;
+       while (ir.hasNext()) {
 			Integer e = (Integer) ir.next();
 			answer[i++] = e;
 		}
-        return answer;
-    }
+       return answer;
+   }
 }
+
+// class Solution {
+// 	public int[] solution(int[] numbers) {
+		
+// 		Set<Integer> set = new HashSet<Integer>();
+		
+// 		for (int i = 0; i < numbers.length; i++) {
+// 			for (int j = i+1; j < numbers.length; j++) {
+// 				set.add(numbers[i]+numbers[j]);
+// 			}
+// 		}
+		
+// 		return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+// 	}
+// }
+// class Solution {
+//    public ArrayList<Integer> solution(int[] numbers) {
+//        ArrayList<Integer> al = new ArrayList<Integer>();
+//        Set<Integer> set = new HashSet<Integer>();
+//        for (int i = 0; i < numbers.length; i++) {
+// 			for (int j = i+1; j < numbers.length; j++) {
+// 				set.add(numbers[i]+numbers[j]);
+// 			}
+// 		}
+       
+//        Iterator<Integer> ir = set.iterator();
+//       while (ir.hasNext()) {
+// 		al.add( ir.next() );
+// 	}
+      
+//      Collections.sort(al);
+//      return al;
+//    }
+// }
