3c5dcc8971834854b9560b013d93e9c8de6f0dfb
msgggg1 <msggms1@gmail.com>
2025-05-18 19:28:45 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,22 +1,12 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        int rest = 0;
         
-        if ( a > n ) {
-			return 0 ;
-		} else {
-			while ( n > 0 ) {
-				answer += b * (n/a);
-				if (n%a != 0) rest += n%a;
-				n = b * (n/a) ;
-				if (rest >= a ) {
-					answer += b * (rest/a) ; 
-					n += b * (rest/a);
-					rest %= a;
-				} 
-			}
+        while (n>=a) {
+			answer += b* (n/a);
+			n = n % a + b*(n/a);
 		}
+      
         return answer;
     }
 }
