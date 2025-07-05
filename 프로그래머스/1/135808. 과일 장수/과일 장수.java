a80d428cadb273dca20153625973d47df3acfc67
msgggg1 <msggms1@gmail.com>
2025-07-05 19:25:36 +0900

    [level 1] Title: 과일 장수, Time: 125.51 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 
+// 내 풀이
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
@@ -13,3 +14,20 @@ class Solution {
         return answer;
     }
 }
+
+
+/* sort() 쓰지 않는 더 효율적인 방법
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int[] sc2 = new int[k+1];
+        for(int i=0; i<score.length; i++) sc2[score[i]]++; 
+        int sum=0, mod=0;
+        for(int i=k; i>0; i--){         
+            sc2[i]=sc2[i] + mod;       
+            sum += sc2[i]/m * m * i;    
+            mod = sc2[i]%m;             
+        }
+        return sum;
+    }
+}
+*/
