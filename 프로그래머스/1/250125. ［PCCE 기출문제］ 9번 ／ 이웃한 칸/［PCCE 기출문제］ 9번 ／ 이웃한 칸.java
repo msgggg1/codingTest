e1f399c2c2893e7e4134c7969c1f1578f3f8bacb
msgggg1 <msggms1@gmail.com>
2025-08-25 15:41:02 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.06 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -1,3 +1,29 @@
+// 내 풀이
+class Solution {
+    public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        String color = board[h][w];
+        int hlength = board.length -1;
+        int wwidth = board[h].length -1;
+        if (h > 0 && board[h-1][w].equals(color)) {
+        		answer++;		
+		}
+        if (w > 0 && board[h][w-1].equals(color)) {
+        	answer++;		
+		}
+        if (w < wwidth && board[h][w+1].equals(color)) {
+        	answer++;			
+		}
+        if (h < hlength && board[h+1][w].equals(color)) {
+        	answer++;		
+		}
+               		
+        return answer;
+    }
+}
+
+
+/*
 class Solution {
     public int solution(String[][] board, int h, int w) {
         int answer = 0;
@@ -23,3 +49,4 @@ class Solution {
         return answer;
     }
 }
+*/
