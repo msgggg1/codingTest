4802507ecb9656da9015fdee7955e7efc9424162
msgggg1 <msggms1@gmail.com>
2025-08-28 15:41:04 +0900

    [level 1] Title: 햄버거 만들기, Time: 72.60 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -18,3 +18,34 @@ class Solution {
 
 
 // stack 사용
+/*
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i : ingredient) {
+            stack.push(i);
+
+            // 스택 크기가 4 이상이면 마지막 4개 확인
+            if (stack.size() >= 4) {
+                int size = stack.size();
+                if (stack.get(size-4) == 1 &&
+                    stack.get(size-3) == 2 &&
+                    stack.get(size-2) == 3 &&
+                    stack.get(size-1) == 1) {
+                    
+                    // 햄버거 완성 → 마지막 4개 제거
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+*/
