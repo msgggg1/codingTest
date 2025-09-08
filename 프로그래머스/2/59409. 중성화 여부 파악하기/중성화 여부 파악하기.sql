ce39e2e468ac48fbae5c06ef5c0853dfa30a9035
msgggg1 <msggms1@gmail.com>
2025-09-08 17:19:21 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,4 +1,5 @@
--- 코드를 입력하세요
+-- WHEN INSTR(sex_upon_intake, 'Neutered') > 0 THEN 'O'
+-- WHEN REGEXP_LIKE(sex_upon_intake, '^(Neutered|Spayed)') THEN 'O'
 SELECT ANIMAL_ID, NAME,
         CASE WHEN sex_upon_intake LIKE 'Neutered%' THEN 'O'
             WHEN sex_upon_intake LIKE 'Spayed%' THEN 'O'
