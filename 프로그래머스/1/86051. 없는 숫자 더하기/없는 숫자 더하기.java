5db83c92c88bba553255bb392052ae0311b9396b
msgggg1 <msggms1@gmail.com>
2026-01-10 20:23:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int [] temp = new int[10];
+        for(int i = 0 ; i < numbers.length ; i++){
+            temp[numbers[i]] = 1;
+        }    
+        
+        for(int i = 0 ; i < temp.length ; i++){
+            if(temp[i] != 1){
+                answer += i;
+            }
+        }
+        
+        return answer ;
+        
+        
+    }
+}
