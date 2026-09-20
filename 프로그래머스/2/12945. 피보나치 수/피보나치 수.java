77020f9fdb9439c58cdbb2218f614ba01c377a1a
msgggg1 <msggms1@gmail.com>
2026-09-20 22:51:00 +0900

    [level 2] Title: 피보나치 수, Time: 1.12 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int mod = 1234567 ;
+        int a = 0 ;
+        int b = 1 ;
+        
+        for(int i = 2; i <=n ; i++ ){
+            int c = (a+b)%mod ;
+            a = b ;
+            b = c ;
+        }
+        return b;
+    }
+}
